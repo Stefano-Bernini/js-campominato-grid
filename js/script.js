f355565d7a7d000dac886289c2dff9e03f79a0d2
Stefano-Bernini <stefanobernini26@gmail.com>
2023-06-05 15:22:48 +0200

    FEAT + esercizio completato: numerate le caselle, aggiunto l'event listener, aggiunto il colore di sfondo al click e mostrato in console il numero della casella cliccata

--- js/script.js
+++ js/script.js
@@ -1,15 +1,34 @@
-const button = document.getElementById('play')
-
-button.addEventListener('click', function(){
+// definizione della funzione che crea la griglia di gioco
+function createNewGame(){
     const grid = document.getElementById('grid')
 
     // svuoto la griglia
     grid.innerHTML = '';
 
+    // invoco la funzione create cells per creare le singole caselle di gioco
+    createCells();
+}
+
+// definizione della funzione che crea le caselle della griglia
+function createCells(){
     // genero le caselle da scrivere nella griglia
     for(let i = 0; i < 100; i++){
         const square = document.createElement('div');
         square.classList.add('square');
+        square.innerText = i + 1;
+
+        square.addEventListener('click', function(){
+            this.classList.add('clicked');
+            console.log(this.innerText);
+        })
+
         grid.append(square);
     }
+}
+
+const button = document.getElementById('play')
+
+button.addEventListener('click', function(){
+    // al click del pulsante invoco la funzione che crea la griglia di gioco
+    createNewGame();
 })
